3b9f01eee0206cf19426a799926f703ab2ce2ad7
Vladimir <v.a.komar7@gmail.com>
2019-11-18 14:03:46 +0200

    add sing up

--- masha_crm/urls.py
+++ masha_crm/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
+from django.conf.urls import url
 
-from .views import home_page, about_page, contact_page, example_page
+from .views import home_page, about_page, contact_page, example_page, signup
 
 urlpatterns = [
-    path('', home_page),
+    url(r'^signup/$', signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', home_page, name='home'),
     re_path(r'^about/$', about_page),
     path('contact', contact_page),
     path('example', example_page),
--- masha_crm/views.py
+++ masha_crm/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template.loader import get_template
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 def home_page(request):
